118a01315ad6ed04e3b033572653731fbda8d2a9
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2022-04-29 12:02:47 +0700

    quý

--- src/Components/util/Spotify.js
+++ src/Components/util/Spotify.js
@@ -13,6 +13,8 @@ const Spotify = {
         if(accessTokenMatch && expiresInMatch){
             accessToken = accessTokenMatch[1]
             const expiresIn = Number(expiresInMatch[1])
+            console.log(expiresIn)
+            console.log(accessTokenMatch)
             //This clears the parameters, allowing us to grab token when it expires.
             window.setTimeout(()=> accessToken='',expiresIn*1000)
             window.history.pushState('Access Token',null,'/spotify')
